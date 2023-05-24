db21f62102874de29bb2b240db3fcdb207b1920d
NiGitaMyrGit <201906799@post.au.dk>
2023-05-24 13:52:13 +0200

    final push

--- src/vgg16_indofash.py
+++ src/vgg16_indofash.py
@@ -22,12 +22,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 #load in labels
 import pandas as pd
-from argparse import ArgumentParser
-
-arser = ArgumentParser(description='Logistic Regression Classifier')
-parser.add_argument('-r','--report_path', type=str, default='out/logistic_regression_report.txt',
-                    help='Path to save the classification report')
-args = parser.parse_args()
 
 #help function to plot and save it too
 def plot_history(H, epochs, plot_name):
@@ -36,16 +30,16 @@ def plot_history(H, epochs, plot_name):
     plt.figure(figsize=(12,6))
     plt.suptitle(f"History for Indo-fashion dataset trained on VGG16", fontsize=14)
     plt.subplot(1,2,1)
-    plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
-    plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss", linestyle=":")
+    plt.plot(np.arange(0, epochs), H.history["loss"], label="train")
+    plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_labels", linestyle=":")
     plt.title("Loss curve")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.tight_layout()
     plt.legend()
     plt.subplot(1,2,2)
-    plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
-    plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc", linestyle=":")
+    plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train")
+    plt.plot(np.arange(0, epochs), H.history["val_acc"], label="val_labels", linestyle=":")
     plt.title("Accuracy curve")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
@@ -72,18 +66,14 @@ def run():
     val_df  = pd.DataFrame(val_labels)
     train_df['image_path'] = "in/" + train_df['image_path']
     test_df['image_path'] = "in/" + test_df['image_path']
-    val_df['image_path'] = "in/" + test_df['image_path']
-    # check amount of labes
+    val_df['image_path'] = "in/" + val_df['image_path']
+    #amount of labels
     n_classes = 15
-    # data paths
-    test_path = os.path.join("in", "images", "test")
-    train_path = os.path.join("in", "images", "train")
-    val_path = os.path.join("in", "images", "val")
     img_rows = 224
     img_cols = 224
     batch_size = 128
-    n_testsamples = 75 #there are 7500 all in all, trying on a subset
-    n_train_samples = 912 # there are 91166, tryin on subset
+    n_testsamples = 75 #TODO there are 7500 all in all, trying on a subset
+    n_train_samples = 911 #TODO there are 91166, tryin on subset
 
     #data augmentation
     train_datagen = ImageDataGenerator(rescale=1. / 255,
@@ -93,6 +83,7 @@ def run():
                                        shear_range=0.2,
                                        zoom_range=0.2,
                                        horizontal_flip=True,
+                                       validation_split=0.2,
                                        fill_mode='nearest')
     test_datagen = ImageDataGenerator(rescale=1. / 255)
     val_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -108,7 +99,7 @@ def run():
                                                         batch_size=batch_size,
                                                         class_mode='categorical',
                                                         shuffle=True)
-    print("okaY")
+    print(train_generator)
     test_generator = test_datagen.flow_from_dataframe(dataframe=test_df, 
                                                             x_col="image_path",
                                                             y_col="class_label",
@@ -116,15 +107,15 @@ def run():
                                                             batch_size=batch_size,
                                                             class_mode='categorical',
                                                             shuffle = False)
-    print("Yaaas")
-    val_generator = test_datagen.flow_from_dataframe(dataframe=val_df, 
+    print(test_generator)
+    validation_generator = val_datagen.flow_from_dataframe(dataframe=val_df, 
                                                             x_col="image_path",
                                                             y_col="class_label",
                                                             target_size=(img_rows, img_cols),
                                                             batch_size=batch_size,
                                                             class_mode='categorical',
                                                             shuffle = False)
-    print("loading model")
+    print(validation_generator)
     #load model
     base_model = VGG16(weights='imagenet', 
                 include_top=False,
@@ -138,13 +129,14 @@ def run():
     tf.keras.backend.clear_session()
     # add new classifier layers
     flat1 = Flatten()(base_model.layers[-1].output)
-    bn = BatchNormalization()(flat1)
-    class1 = Dense(256, activation='relu')(bn)
-    class2 = Dense(128, activation='relu')(class1)
-    output = Dense(n_classes, activation='softmax')(class2)
+    x = Dense(256, activation='relu')(flat1)
+    x = Dense(128, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(32, activation='relu')(x)
+    outputs = Dense(15, activation='softmax')(x)
     # define new model
     model = Model(inputs=base_model.inputs, 
-                outputs=output)
+                outputs=outputs)
     # compile
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=0.01,
@@ -153,26 +145,28 @@ def run():
     sgd = SGD(learning_rate=lr_schedule)
     print("model compile")
     model.compile(optimizer=sgd,
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'],
+                  run_eagerly=True)
 
     # stop training if validation loss i 0 for 2 epochs
-    callback  = [EarlyStopping(monitor='val_loss',
+    callbacks  = [EarlyStopping(monitor='val_loss',
                                    min_delta=0,
-                                   patience=2, 
+                                   patience=2, #TODO set to 2
                                    mode='auto')]
 
     H = model.fit_generator(train_generator,
                             steps_per_epoch=n_train_samples//batch_size,
-                            epochs = 5,
-                            validation_data=val_generator,
+                            epochs = 3, #TODO set to 5
+                            validation_data=validation_generator,
                             validation_steps=n_testsamples//batch_size,
-                            callbacks = callback)
-    print("val_generator resets")              
-    val_generator.reset()
+                            callbacks = callbacks)
+
+    print("validation_generator resets")              
+    validation_generator.reset()
     print("validating model")
     # evaluate model
-    Y_pred = model.predict(val_generator, steps=len(test_generator))
+    Y_pred = model.predict(validation_generator, steps=len(test_generator))
     y_pred = np.argmax(Y_pred, axis=1)
     print('Classification Report')
     # get labels
@@ -185,7 +179,7 @@ def run():
     # get actual number of epochs used
     n_epochs = len(H.history['loss'])
     # save report
-    save_report(report, "classification_report", 0.01,5, 128)
+    save_report(report, "classification_report", 0.01,3,128)
     # save history plot
     plot_history(H, n_epochs, "history plot")
 
